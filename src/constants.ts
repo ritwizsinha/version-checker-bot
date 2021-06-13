48c0f20a5d9eb9a37d6b511ae0400d0bd86f6a37
Ritwiz Sinha <ritwizsinha0@gmail.com>
2021-06-13 08:34:41 +0530

    Adding sonar test check

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+const greater_version_number_than_master = 'The version of package on this branch should be greater than master'
+const error_in_checking_versions = 'Error in checking versions for this branch';
+const feature_testing_pom = 'testing-pom-version-in-pr-check';
+const feature_create_tag_on_merge = 'create-tag-on-merge';
+const feature_did_sonar_run = 'did-run-sonar-check';
+
+export {
+    greater_version_number_than_master,
+    error_in_checking_versions,
+    feature_create_tag_on_merge,
+    feature_testing_pom,
+    feature_did_sonar_run
+}
---
+++ src/functions/usecases.ts
@@ -0,0 +1,69 @@
+import { getVersionFromRefWithContext } from './utilities';
+import { greater_version_number_than_master, error_in_checking_versions } from '../constants';
+import { Context } from 'vm';
+
+export async function checkPRVersionWithMain(context): Promise<string> {
+    const { ref } = context.payload.pull_request.head
+    const owner = context.payload.repository.owner.login as string;
+    const repoName = context.payload.repository.name;
+    try {
+        const versionStringOnPr = await getVersionFromRefWithContext(context, { repo: repoName, owner, ref });
+        const versionStringOnMaster = await getVersionFromRefWithContext(context, { repo: repoName, owner, ref: null });
+
+        console.log(versionStringOnMaster, versionStringOnPr);
+        if (versionStringOnPr <= versionStringOnMaster) {
+            return greater_version_number_than_master;
+        }
+        return "";
+    }
+    catch (err) {
+        console.log(err);
+        return error_in_checking_versions;
+    }
+}
+
+export async function createTagOnMergeToMain(context) {
+    const { merge_commit_sha: mergeCommitSha } = context.payload.pull_request;
+    const owner = context.payload.repository.owner.login as string;
+    const repoName = context.payload.repository.name;
+    try {
+      const mainBranchVersion = await getVersionFromRefWithContext(context, { repo: repoName, owner, ref: null });
+
+      const { data: { sha } } = await context.octokit.rest.git.createTag({
+        tag: `v${mainBranchVersion}`,
+        object: mergeCommitSha,
+        message: "Creating tag on merge",
+        tagger: {
+          name: owner,
+          email: "abc@gmail.com",
+        },
+        type: 'commit',
+        owner,
+        repo: repoName,
+      });
+
+      await context.octokit.rest.git.createRef({
+        owner,
+        repo: repoName,
+        ref: `refs/tags/v${mainBranchVersion}`,
+        sha,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+}
+
+export async function didSonarTestRunAndPass(context: Context) {
+
+  // Is the check turned on
+   
+  // Get the config file containing project key
+
+  // Get the project key
+
+  // Call the sonar api
+
+  // Get the status
+
+  // If status is failing return a comment with the details
+}
