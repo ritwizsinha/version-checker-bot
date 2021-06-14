914dddb77eb10c36608226ec4d8214b7f94ceeb3
Ritwiz Sinha <ritwizsinha0@gmail.com>
2021-06-14 14:19:43 +0530

    Some more changes

--- src/index.ts
+++ src/index.ts
@@ -4,43 +4,49 @@ import { feature_testing_pom, feature_create_tag_on_merge, feature_did_sonar_run
 
 import { load } from 'js-yaml';
 
-export = (app: Probot) => {
-  app.on(['pull_request.opened', 'pull_request.reopened'], async (context) => {
-    const config = (await context.config('bot.yml')) as any;
-    const shouldRun = config[feature_testing_pom]['detect'];
-    if (!shouldRun) return;
-
-    const givenBranches = config[feature_testing_pom]['branches'] as Array<{ from: string, to: string }>;
-    const { head: { ref: fromBranch }, base: { ref: toBranch } } = context.payload.pull_request;
-    if (givenBranches.filter((item) => item.from === fromBranch && item.to === toBranch).length === 0) return;
-
-    const commentText = await checkPRVersionWithMain(context);
-    if (commentText === "") return;
-    const comment = context.issue({
-      body: commentText,
-    });
-    await context.octokit.issues.createComment(comment);
-
-
-    // Checking for sonar test
-    const shouldRunSonarTest = config[feature_did_sonar_run]['detect'];
-    if (!shouldRunSonarTest) return;
-
-    const projectKeyFile = config[feature_did_sonar_run]['project-key-file'];
-    const owner = context.payload.repository.owner.login as string;
-    const repoName = context.payload.repository.name;
-    const params = {
-      owner,
-      path: projectKeyFile,
-      repo: repoName,
-      mediaType: {
-        format: 'raw'
+export default (app: Probot) => {
+  app.on(['pull_request.opened', 'pull_request.reopened', 'pull_request.synchronize'], async (context) => {
+    try {
+      const config = (await context.config('bot.yml')) as any;
+      console.log(config);
+      const shouldRun = config[feature_testing_pom]['detect'];
+      if (!shouldRun) return;
+  
+      const givenBranches = config[feature_testing_pom]['branches'] as Array<{ from: string, to: string }>;
+      const { head: { ref: fromBranch }, base: { ref: toBranch } } = context.payload.pull_request;
+      if (givenBranches.filter((item) => item.from === fromBranch && item.to === toBranch).length === 0) return;
+  
+      const commentText = await checkPRVersionWithMain(context);
+      if (commentText === "") return;
+      const comment = context.issue({
+        body: commentText,
+      });
+      await context.octokit.issues.createComment(comment);
+  
+  
+      // Checking for sonar test
+      const shouldRunSonarTest = config[feature_did_sonar_run]['detect'];
+      if (!shouldRunSonarTest) return;
+  
+      const projectKeyFile = config[feature_did_sonar_run]['project-key-file'];
+      const owner = context.payload.repository.owner.login as string;
+      const repoName = context.payload.repository.name;
+      const params = {
+        owner,
+        path: projectKeyFile,
+        repo: repoName,
+        mediaType: {
+          format: 'raw'
+        }
       }
+  
+      const { data } = await context.octokit.repos.getContent(params);
+      const yamlJSON = load(data.toLocaleString());
+      console.log(yamlJSON);
+    } catch (err) {
+      console.log(err);
     }
-
-    const { data } = await context.octokit.repos.getContent(params);
-    const yamlJSON = load(data.toLocaleString());
-    console.log(yamlJSON);
+    
   });
 
   app.on('pull_request.closed', async (context) => {
